55160028f0dc85401424faa669e660f3c7e71d45
ezekielhutagalung <yehezkiel.life@gmail.com>
2021-03-21 12:39:19 +0700

    clientNavigationFix

--- socio1/App.js
+++ socio1/App.js
@@ -2,14 +2,16 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import NavigationPage from "./src/navigation/navigation";
-
+import { store } from "./src/redux/store";
+import { Provider } from "react-redux";
 
 const AppStack = createStackNavigator();
 
 export default function App() {
   const image = { uri: "https://reactjs.org/logo-og.png" };
   return (
-    <NavigationPage />
-   
+    <Provider store={store}>
+      <NavigationPage />
+    </Provider>
   );
 }
